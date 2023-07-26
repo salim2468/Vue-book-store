205ef26c8c6a9cb3252a5077ca5487df1f7a6150
salim2468 <salimmaharjan2468@gmail.com>
2023-07-26 11:15:33 +0545

    side bar design issue fixed

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,25 +1,50 @@
 <template>
   <div class="side-bar">
     <ul>
-      <li>
-        <router-link :to="{name:'Home'}" class="router-link">
-          <span class="material-symbols-outlined"> import_contacts </span> All
-          Books</router-link
-        >
-      </li>
-      <li>
-        <router-link :to="{name:'AddBook'}" class="router-link">
-          <span class="material-symbols-outlined"> add_circle </span>
-          <span> Add
-          Book</span></router-link
-        >
-      </li>
-      <li>
-        <router-link :to="{name:'SearchBook'}" class="router-link"> 
-          <span class="material-symbols-outlined"> search </span>Search
-          Books</router-link
-        >
-      </li>
+      <li><h6 class="title">Book</h6></li>
+      <ul>
+        <li>
+          <router-link :to="{ name: 'Home' }" class="router-link">
+            <span class="material-symbols-outlined"> import_contacts </span> All
+            Books</router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'AddBook' }" class="router-link">
+            <span class="material-symbols-outlined"> add_circle </span>
+            <span> Add Book</span></router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'SearchBook' }" class="router-link">
+            <span class="material-symbols-outlined"> search </span>Search
+            Books</router-link
+          >
+        </li>
+      </ul>
+      <hr>
+      <li><h6 class="title">Author</h6></li>
+      <ul>
+        <li>
+          <router-link :to="{ name: 'Home' }" class="router-link">
+            <span class="material-symbols-outlined"> import_contacts </span> All
+            Authors</router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'AddBook' }" class="router-link">
+            <span class="material-symbols-outlined"> add_circle </span>
+            <span> Add Author</span></router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'SearchBook' }" class="router-link">
+            <span class="material-symbols-outlined"> search </span>Search
+            Author</router-link
+          >
+        </li>
+      </ul>
+      <hr>
     </ul>
   </div>
 </template>
@@ -37,6 +62,8 @@ export default {
   background: #f8f8f8;
   margin-right: 10px;
   margin-top: 2px;
+  padding-top: 20px;
+  padding-right: 20px;
 }
 /* .background-circle{
   background: grey;
@@ -47,11 +74,16 @@ li {
   display: flex;
   align-items: center;
 }
-.router-link{
+.router-link {
   text-decoration: none;
   color: black;
+  display: flex;
+  margin-top: 4px;
 }
 .material-symbols-outlined {
   margin-right: 10px; /* Add some spacing between the icon and the heading */
+}
+.title{
+  background:#DCDCDC;padding:4px 15px;border-radius:14px;
 }
 </style>
